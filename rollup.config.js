c3e3cd3510e776b992c3029d4ba92440b12102f7
Josh Kolenko <joshua.kolenko@wwwinc.com>
2024-03-18 14:11:19 -0400

    refactor: move esm bundling to tsc

--- rollup.config.js
+++ rollup.config.js
@@ -7,49 +7,32 @@ import { nodeResolve } from '@rollup/plugin-node-resolve';
 
 const { npm_package_name, npm_package_version } = process.env;
 
-const name = 'saySomething';
+const name = 'saySomething'; // ! Don't forget to change this. This is the name of the global variable
 const banner = `/** ${npm_package_name} @version ${npm_package_version} */\n`;
 
-const input = 'src/index.ts';
-
-export default [
-  {
-    // module
-    input,
-    output: {
+export default {
+  input: 'src/index.ts',
+  output: [
+    {
+      name,
       banner,
-      format: 'esm',
-      preserveModules: true,
-      dir: path.resolve('dist'),
+      format: 'umd',
+      file: path.resolve('dist', 'browser', 'index.js'),
+      sourcemap: true,
     },
-
-    plugins: [
-      nodeResolve(),
-      typescript({
-        declaration: true,
-        declarationDir: path.resolve('dist'),
-      }),
-    ],
-  },
-  {
-    // browser
-    input,
-    output: [
-      {
-        name,
-        banner,
-        format: 'umd',
-        file: path.resolve('dist', 'browser', 'index.js'),
-        sourcemap: true,
-      },
-      {
-        name,
-        banner,
-        format: 'umd',
-        file: path.resolve('dist', 'browser', 'index.min.js'),
-        plugins: [terser()],
-      },
-    ],
-    plugins: [nodeResolve(), babel({ babelHelpers: 'bundled' }), typescript()],
-  },
-];
+    {
+      name,
+      banner,
+      format: 'umd',
+      file: path.resolve('dist', 'browser', 'index.min.js'),
+      plugins: [terser()],
+    },
+  ],
+  plugins: [
+    nodeResolve(),
+    babel({ babelHelpers: 'bundled' }),
+    typescript({
+      declaration: false,
+    }),
+  ],
+};
